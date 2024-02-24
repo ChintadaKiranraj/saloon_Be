c60f3b383841c2bb6cc830a5a64080c89be0f079
yogendra.kola <yogendra.kola@epsoftinc.com>
2024-02-24 20:34:11 +0530

    bootking API's

--- src/students/studentsql.js
+++ src/students/studentsql.js
@@ -4,6 +4,9 @@ const insertInLogin = "INSERT INTO admin(name,number) VALUES($1,$2) RETURNING *"
 const fetchLoginDetails = "SELECT * FROM admin";
 const saveRegistration = "INSERT INTO registration (email_id,first_name,last_name,phone_number,password,confirm_password) VALUES($1,$2,$3,$4,$5,$6) RETURNING *";
 const validateLoginUser = "SELECT * FROM registration WHERE email_id = $1 AND password = $2";
+const fetchBookingDetails = "select * from booking_details";
+const saveBookingDetails = "INSERT INTO booking_details (name,date,time,status) VALUES($1, $2, $3, $4) RETURNING *";
+const updateBookingDetails = "UPDATE booking_details SET status = $1 WHERE id =$2 RETURNING *"; 
 module.exports = {
   getStudentsList,
   getStudentById,
@@ -11,4 +14,7 @@ module.exports = {
   insertInLogin,
   saveRegistration,
   validateLoginUser,
+  fetchBookingDetails,
+  saveBookingDetails,
+  updateBookingDetails,
 };
